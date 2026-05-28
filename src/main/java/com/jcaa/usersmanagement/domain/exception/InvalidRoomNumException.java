f55b8534400bbca113034e2c5db4a195ea4d445e
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-05-28 00:05:12 -0500

    chore: definiendo RoomModel y creacion de excepcion InvalidRoomException

---
+++ src/main/java/com/jcaa/usersmanagement/domain/exception/InvalidRoomNumException.java
@@ -0,0 +1,7 @@
+package com.jcaa.usersmanagement.domain.exception;
+
+public class InvalidRoomNumException  extends DomainException{
+    public InvalidRoomNumException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/jcaa/usersmanagement/domain/model/RoomModel.java
+++ src/main/java/com/jcaa/usersmanagement/domain/model/RoomModel.java
@@ -1,4 +1,22 @@
 package com.jcaa.usersmanagement.domain.model;
 
+import com.jcaa.usersmanagement.domain.enums.RoomStatus;
+import com.jcaa.usersmanagement.domain.valueobject.RoomName;
+import com.jcaa.usersmanagement.domain.valueobject.RoomNum;
+
 public class RoomModel {
+    private RoomName roomName;
+    private RoomNum roomNum;
+    private RoomStatus roomStatus;
+
+    private RoomModel(RoomName roomName, RoomNum roomNum, RoomStatus roomStatus) {
+        this.roomName = roomName;
+        this.roomNum = roomNum;
+        this.roomStatus = roomStatus;
+    }
+
+    public static RoomModel factoryRoom(RoomName name, RoomNum num, RoomStatus status){
+      return  new RoomModel(name, num, status);
+    }
+
 }
