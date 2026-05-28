2da6ae15ea9e73589dd18659b6cbb4d3d231c473
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-05-28 14:43:00 -0500

    feat: creacion de logica del dto CreateRoomCommand y logica de CreateRoomUseCase

--- src/main/java/com/jcaa/usersmanagement/application/service/dto/command/CreateRoomCommand.java
+++ src/main/java/com/jcaa/usersmanagement/application/service/dto/command/CreateRoomCommand.java
@@ -1,4 +1,9 @@
 package com.jcaa.usersmanagement.application.service.dto.command;
 
-public record CreateRoomCommand() {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record CreateRoomCommand(
+        @NotNull(message = "El número de sala es obligatorio") Integer number,
+        @NotBlank(message = "El nombre de la sala no puede estar vacío") String name
+) {}
