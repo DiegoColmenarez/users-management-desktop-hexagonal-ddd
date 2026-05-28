9ca1b1df281c2ab06334a8d9f7b73bf24e993a06
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-05-28 17:22:45 -0500

    chore: Cambio de nombre del metodo excute de GetAllRoomsUseCase

--- src/main/java/com/jcaa/usersmanagement/application/port/in/GetAllRoomsUseCase.java
+++ src/main/java/com/jcaa/usersmanagement/application/port/in/GetAllRoomsUseCase.java
@@ -5,5 +5,5 @@ import com.jcaa.usersmanagement.domain.model.RoomModel;
 import java.util.List;
 
 public interface GetAllRoomsUseCase {
-    List<RoomModel> excute();
+    List<RoomModel> getAll();
 }
