d88641262be2624d99478c6883b2c38df47da557
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-05-28 00:46:16 -0500

    chore: implementar Value Object RoomNum para validación de datos e implementacionInvalidRoomException

--- src/main/java/com/jcaa/usersmanagement/domain/exception/InvalidRoomNumException.java
+++ src/main/java/com/jcaa/usersmanagement/domain/exception/InvalidRoomNumException.java
@@ -4,4 +4,12 @@ public class InvalidRoomNumException  extends DomainException{
     public InvalidRoomNumException(String message) {
         super(message);
     }
+    private static final String MESSAGE_NEGATIVE_NUM = "EL numero asignado es negativo, no es valido.";
+    private static final String MESSAGE_NULL_ARGUMENT= "EL numero asignado es NULL, no es valido.";
+    public static InvalidRoomNumException numIsNegative(){
+        return new InvalidRoomNumException(MESSAGE_NEGATIVE_NUM);
+    }
+    public static InvalidRoomNumException numIsNull(){
+        return new InvalidRoomNumException(MESSAGE_NULL_ARGUMENT);
+    }
 }
--- src/main/java/com/jcaa/usersmanagement/domain/valueobject/RoomNum.java
+++ src/main/java/com/jcaa/usersmanagement/domain/valueobject/RoomNum.java
@@ -1,4 +1,22 @@
 package com.jcaa.usersmanagement.domain.valueobject;
 
-public record RoomNum() {
+import com.jcaa.usersmanagement.domain.exception.InvalidRoomNumException;
+import com.jcaa.usersmanagement.domain.exception.InvalidUserIdException;
+
+public record RoomNum(Integer num) {
+    public RoomNum {
+        validateNotNull(num);
+        validateNotNegative(num);
+    }
+
+    private static void validateNotNull(final Integer num) {
+        if (num == null) {
+            throw InvalidRoomNumException.numIsNull();
+        }
+    }
+    private static void validateNotNegative(final Integer num) {
+        if (num < 1) {
+            throw InvalidRoomNumException.numIsNegative();
+        }
+    }
 }
