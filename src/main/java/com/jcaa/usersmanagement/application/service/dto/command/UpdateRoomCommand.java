320a2e6708d2d79e0f4a6c511c5525f25d02de20
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-05-28 21:25:12 -0500

    feat: Creacion de la logica de UpdateRoomCommand (DTO) y UpdateRoom (Port y UseCase)

--- src/main/java/com/jcaa/usersmanagement/application/service/dto/command/UpdateRoomCommand.java
+++ src/main/java/com/jcaa/usersmanagement/application/service/dto/command/UpdateRoomCommand.java
@@ -1,4 +1,9 @@
 package com.jcaa.usersmanagement.application.service.dto.command;
 
-public record UpdateRoomCommand() {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record UpdateRoomCommand(
+        @NotNull(message = "El número de la sala es obligatorio") Integer number,
+        @NotBlank(message = "El nuevo nombre no puede estar vacío") String newName
+) {}
