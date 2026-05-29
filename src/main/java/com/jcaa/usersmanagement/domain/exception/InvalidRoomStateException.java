58d542732825847b302a7e5a6481689f329e4aee
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-05-28 23:52:25 -0500

    chore: creacion de invalidRoomStateException

---
+++ src/main/java/com/jcaa/usersmanagement/domain/exception/InvalidRoomStateException.java
@@ -0,0 +1,7 @@
+package com.jcaa.usersmanagement.domain.exception;
+
+public class InvalidRoomStateException extends  DomainException{
+    public InvalidRoomStateException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/jcaa/usersmanagement/domain/model/RoomModel.java
+++ src/main/java/com/jcaa/usersmanagement/domain/model/RoomModel.java
@@ -3,9 +3,10 @@ package com.jcaa.usersmanagement.domain.model;
 import com.jcaa.usersmanagement.domain.enums.RoomStatus;
 import com.jcaa.usersmanagement.domain.valueobject.RoomName;
 import com.jcaa.usersmanagement.domain.valueobject.RoomNum;
+import com.jcaa.usersmanagement.domain.exception.InvalidRoomStateException;
 
 public class RoomModel {
-    private final RoomName roomName;
+    private RoomName roomName;
     private final RoomNum roomNum;
     private RoomStatus roomStatus;
 
@@ -19,10 +20,25 @@ public class RoomModel {
         return new RoomModel(name, num, RoomStatus.ENABLED);
     }
 
+    public static RoomModel reconstitute(RoomName name, RoomNum num, RoomStatus status) {
+        return new RoomModel(name, num, status);
+    }
+
+    public void rename(RoomName newName) {
+        this.roomName = newName;
+    }
+
     public void enable() {
+        if (this.roomStatus == RoomStatus.ENABLED) {
+            throw new InvalidRoomStateException("The room is already enabled. Redundant transition attempted for room: " + this.roomNum.num());
+        }
         this.roomStatus = RoomStatus.ENABLED;
     }
+
     public void disable() {
+        if (this.roomStatus == RoomStatus.DISABLED) {
+            throw new InvalidRoomStateException("The room is already disabled. Redundant transition attempted for room: " + this.roomNum.num());
+        }
         this.roomStatus = RoomStatus.DISABLED;
     }
 
@@ -33,6 +49,7 @@ public class RoomModel {
     public Integer roomNum() {
         return this.roomNum.num();
     }
+
     public RoomStatus roomStatus(){
         return this.roomStatus;
     }
