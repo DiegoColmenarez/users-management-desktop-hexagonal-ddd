438bae99b0592304228517513df9d4ef81d5ded5
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2026-05-29 23:17:00 -0500

    fix: Cambiando @notblank por @notnull por falla al ejecutar comandos

--- src/main/java/com/jcaa/usersmanagement/application/service/dto/command/DeleteRoomCommand.java
+++ src/main/java/com/jcaa/usersmanagement/application/service/dto/command/DeleteRoomCommand.java
@@ -1,8 +1,8 @@
 package com.jcaa.usersmanagement.application.service.dto.command;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record DeleteRoomCommand(
-        @NotBlank(message = "El número de la sala no puede estar vacío")
+        @NotNull(message = "El número de la sala no puede estar vacío")
         Integer number
 ) {}
